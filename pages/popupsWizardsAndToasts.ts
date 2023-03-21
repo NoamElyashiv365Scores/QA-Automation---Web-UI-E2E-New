eb9100f1912941ef17bfcbc1a371c55f9ad60fe8
noam <noam.elyashiv@365scores.com>
2023-03-21 18:31:29 +0200

    reverted cookie path

--- pages/popupsWizardsAndToasts.ts
+++ pages/popupsWizardsAndToasts.ts
@@ -1,16 +1,13 @@
 import { expect, Locator, Page } from "@playwright/test";
-export class PopupsWizardsAndToasts {
+export class PopupsAndToasts {
   readonly page: Page;
   readonly cookiesWizard: Locator;
   readonly faveLeaguePopup: Locator;
   constructor(page: Page) {
     this.page = page;
-    this.cookiesWizard = page
-      /*.locator(
+    this.cookiesWizard = page.locator(
       "xpath=//div[contains(@class, 'popup')]//button[contains(@id, 'agree')]"
-    );*/ .getByRole("button", {
-        name: "Agree and close: Agree to our data processing and close",
-      });
+    );
     this.faveLeaguePopup = page.locator(
       "css=div[class*='wizard-widget'] > button"
     );
