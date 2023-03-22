47ebf1e7534449c6d2d25ca58eec13c80efce317
noam <noam.elyashiv@365scores.com>
2023-03-22 15:40:39 +0200

    added ip param

--- tests/sportOrderTest.spec.ts
+++ tests/sportOrderTest.spec.ts
@@ -4,11 +4,11 @@ import { Header } from "../pages/header";
 test("sportOrderTest", async ({ page }) => {
   const popupsWizardsAndToasts = new PopupsWizardsAndToasts(page);
   //goes to the English (UK) version of the site
-  await page.goto("https://365scores.com/en-uk");
-  /* await test.step(`Closes the "Accept cookies" wizard`, async () => {
+  await page.goto("https://365scores.com/en-uk/?ipParam=103.204.124.0");
+  await test.step(`Closes the "Accept cookies" wizard`, async () => {
     await popupsWizardsAndToasts.closeCookiesWizard();
   });
-*/
+
   await test.step(`Closes the Favorite league popup`, async () => {
     await popupsWizardsAndToasts.closeFaveLeaguePopup();
   });
